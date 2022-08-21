693b8f1efa9ddabb134550331b05604afd264813
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-21 12:39:45 -0300

    :sparkles: create: fun - getCaracterVerifier

--- src/verifier/verifier.ts
+++ src/verifier/verifier.ts
@@ -1,8 +1,21 @@
-import { IRules, IVerifier } from '@Types';
-export function getVerifier(rules: IRules): IVerifier {
+import { ICaracterRules, IRules, IVerifier } from '@Types';
+export function getVerifier(rules: IRules): IVerifier<IRules> {
     return {
         rule: rules,
         verify(content) {
+            getCaracterVerifier(this.rule.caracters!)
+        },
+        apply(content) {
+
+        }
+    }
+}
+
+export function getCaracterVerifier(rules: ICaracterRules): IVerifier<ICaracterRules> {
+    return {
+        rule: rules,
+        verify(content) {
+            
             
         },
         apply(content) {
