f10214373d0c98fbb9cbc29d354147f33e0f8698
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-21 15:00:12 -0300

    :sparkles: create: fun - applyCaracterRules

--- src/verifier/apply.ts
+++ src/verifier/apply.ts
@@ -1,6 +1,6 @@
 import { RULE_KEYS } from "@Consts";
-import { IApplyResult, IRules } from "@Types";
-import { verify } from "./verify";
+import { IApplyResult, ICaracterRules, IRules } from "@Types";
+import { verify, verifyCaractersRules } from './verify';
 
 export function apply(rules: IRules, content:string): IApplyResult {
     const result = verify(content, rules);
@@ -8,14 +8,21 @@ export function apply(rules: IRules, content:string): IApplyResult {
         if(!result.get(RULE_KEYS.fixed_length)) content = content.slice(0,rules.fixed_length);
         if(!result.get(RULE_KEYS.max_length)) content = content.slice(0, rules.max_length);
         
-        if(!result.get(RULE_KEYS.caracters.all_lowercase)) content = rules.caracters?.all_lowercase? content.toLocaleLowerCase() : content;
-        if(!result.get(RULE_KEYS.caracters.all_uppercase)) content = rules.caracters?.all_uppercase? content.toLocaleUpperCase() : content;
-
-        rules.caracters?.dont_have?.forEach(caracter => {if(!result.get(RULE_KEYS.caracters.dont_have(caracter))) content = content.replace(caracter, "")})
-        rules.caracters?.must_have?.forEach(caracter => {if(!result.get(RULE_KEYS.caracters.must_have(caracter))) content = content.concat(caracter)})
-
+        
         return {result: content, matchs: result}
     } catch (error) {
         return {matchs: new Map<string, boolean>().set("ERROR",true), result: content}
     }
+}
+
+export function applyCaracterRules(rules: ICaracterRules, content: string, result?:Map<string, boolean>): IApplyResult{
+    const results = result? result : verifyCaractersRules(rules, content)
+
+    if(!results.get(RULE_KEYS.caracters.all_lowercase)) content = rules.all_lowercase? content.toLocaleLowerCase() : content;
+    if(!results.get(RULE_KEYS.caracters.all_uppercase)) content = rules.all_uppercase? content.toLocaleUpperCase() : content;
+
+    rules.dont_have?.forEach(caracter => {if(!results.get(RULE_KEYS.caracters.dont_have(caracter))) content = content.replace(caracter, "")})
+    //rules.must_have?.forEach(caracter => {if(!results.get(RULE_KEYS.caracters.must_have(caracter))) content = content.concat(caracter)})
+    
+    return {result: content, matchs: results};
 }
