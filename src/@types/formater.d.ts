59d1fa10e707a2fd16fd5e087eb5b9db9ff5dea0
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-21 11:45:21 -0300

    :recycle: refact: IFormater

--- src/@types/formater.d.ts
+++ src/@types/formater.d.ts
@@ -1,6 +1,7 @@
+import { IRange } from './range';
 export interface IFormater {
     GROUP?: (constent: string) => string
-    ALLOW_LETTER?: (letters: range | string, allowUppercase: boolean) => string
-    ALLOW_NUMBER?: (numbers: range | number) => string
+    ALLOW_LETTER?: (letters: IRange<string> | string, allowUppercase: boolean) => string
+    ALLOW_NUMBER?: (numbers: IRange<number> | number) => string
     ALLOW_SPECIAL_CHARACTER?: (caracters: string[]) => string
 }
