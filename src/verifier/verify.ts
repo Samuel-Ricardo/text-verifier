d9ed96342531420903ac1f836fa8509d47a219e7
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-21 14:01:32 -0300

    :sparkles: create: fun - verifyCaractersRules

--- src/verifier/verify.ts
+++ src/verifier/verify.ts
@@ -1,5 +1,5 @@
 import { RULE_KEYS } from "@Consts";
-import { IRules } from "@Types";
+import { ICaracterRules, IRules } from "@Types";
 
 export function verify(content:string, rules:IRules):Map<string, boolean> {
     var results = new Map<string, boolean>();
@@ -17,4 +17,19 @@ export function verify(content:string, rules:IRules):Map<string, boolean> {
     if(min_length) results.set(RULE_KEYS.min_length, content.length >= min_length)
 
     return results;
+}
+
+export function verifyCaractersRules (rules: ICaracterRules, content: string){
+    const result = new Map<string,boolean>();
+    const { caracters } = RULE_KEYS;
+
+    if(rules) {
+        if(rules.all_lowercase) result.set(caracters.all_lowercase, content.toLowerCase() === content)
+        if(rules.all_uppercase) result.set(caracters.all_uppercase, content.toUpperCase() === content)
+
+        if(rules.dont_have) rules.dont_have.forEach(caracter => result.set(caracters.dont_have(caracter), !content.includes(caracter)))
+        if(rules.must_have) rules.must_have.forEach(caracter => result.set(caracters.must_have(caracter), content.includes(caracter)))
+    }
+
+    return result;
 }
