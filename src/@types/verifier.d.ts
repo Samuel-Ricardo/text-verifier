8fb94231c74e89e7327711544c2546889889b6bf
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-21 14:44:17 -0300

    :recycle: clean

--- src/@types/verifier.d.ts
+++ src/@types/verifier.d.ts
@@ -1,8 +1,4 @@
-import { IVerifiable } from './verifieable';
-import { IApplicable } from './applicable';
-import { IRules } from './rule/rules';
-import { ICaracterRules } from './rule/caracter-rules';
-import { IFormat } from './rule';
+import { IVerifiable, IApplicable, IRules, ICaracterRules, ICaracterRules, IFormat } from './index';
 export interface IVerifier<rules = IRules | ICaracterRules | IFormat> extends IVerifiable, IApplicable {
     rule: rules
 }
