e94bb7faae45bea34de8d006b11d731b2c392d13
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-21 12:12:08 -0300

    :sparkles: create: fun - filterEspecialCaracters (@utils)

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -19,3 +19,5 @@ export const filterSpecialCaracter = (caracter:string) => {
 }
 
 export const filterSpecialCaracterOrIgnore = (caracter: string, ignore: boolean) => ignore? caracter : filterSpecialCaracter(caracter) 
+
+export const filterEspecialCaracters = (caracter:string[]) => caracter.map((caracter) => filterSpecialCaracter(caracter))
