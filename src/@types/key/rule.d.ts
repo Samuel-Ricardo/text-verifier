46927b34cc9ad419ffd474d736568cc40a78abf6
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-21 11:27:45 -0300

    :sparkles: update : interface - IDefaultRuleKey (@types/key)

--- src/@types/key/rule.d.ts
+++ src/@types/key/rule.d.ts
@@ -1,4 +1,4 @@
-import { ICaracterRuleKey } from "./caracter";
+import { ICaracterRuleKey, IDefaultCaracterRuleKey } from './caracter';
 
 export interface IRuleKey {
     caracters?: ICaracterRuleKey,
@@ -6,4 +6,12 @@ export interface IRuleKey {
     min_length?:string,
     max_length?:string,
     fixed_length?:string,
+}
+
+export interface IDefaultRuleKey extends IRuleKey {
+    caracters?: IDefaultCaracterRuleKey,
+    format?: `format_matches`,
+    min_length?: `min_length`,
+    max_length?: `max_length`,
+    fixed_length?: `fixed_length`,
 }
