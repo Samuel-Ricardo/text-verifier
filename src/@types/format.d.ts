3685d05f505475794b279b0967ff09d472680be3
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-21 10:40:58 -0300

    :sparkles: update : type - IFormat (@types)

--- src/@types/format.d.ts
+++ src/@types/format.d.ts
@@ -1,4 +1,7 @@
-export interface IFormat {
+import { IApplicable } from "./applicable";
+import { IVerifiable } from "./verifieable";
+
+export interface IFormat extends IVerifiable, IApplicable{
     string_to_replace: IReplacebleString[],
     model: string,
 }
