710b99bf975f035f739eba5167a02f4944315d07
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-21 11:19:06 -0300

    :sparkles: update : interface - IDefaultCaracterRuleKey (@types/key)

--- src/@types/key/caracter.d.ts
+++ src/@types/key/caracter.d.ts
@@ -4,3 +4,10 @@ export interface ICaracterRuleKey {
     all_uppercase: string;
     all_lowercase: string;
 }
+
+export interface IDefaultCaracterRuleKey extends ICaracterRuleKey {
+    must_have: (caracter:string) => `must_have - ${caracter}`;
+    dont_have: (caracter:string) => `dont_have - ${caracter}`;
+    all_uppercase: `all_uppercase`;
+    all_lowercase: `all_lowercase`
+}
