f1ddfcdb6d9841ca63e452909d0a4b8171b4634e
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-25 23:01:58 -0300

    :recycle: clean: file - formater.d.ts

--- src/@types/formater.d.ts
+++ src/@types/formater.d.ts
@@ -1,32 +1,16 @@
 import { IRange } from './range';
 import { apply } from '../verifier/apply';
+import {IGroupProps, IAllowLetterProps, IAllowNumberProps, IAllowSpecialCharacterProps} from './index'
+
 export interface IFormaterRules {
     GROUP?: IGroupProps
     ALLOW_LETTER?: IAllowLetterProps
-    ALLOW_NUMBER?: AllowNumberProps
+    ALLOW_NUMBER?: IAllowNumberProps
     ALLOW_SPECIAL_CHARACTER?: IAllowSpecialCharacterProps
 }
 export interface IFormater {
-    GROUP?: (content: string, range: IRange<number>) => string
+    GROUP?: (content: string, groupes_range: IRange<number>[]) => string
     ALLOW_LETTER?: (letters: IRange<string> | string, allowUppercase: boolean) => string
     ALLOW_NUMBER?: (numbers: IRange<number> | number) => string
     ALLOW_SPECIAL_CHARACTER?: (caracters: string[]) => string
-}
-
-export type IGroupProps = {
-    content: string,
-    range: IRange<number>
-}
-
-export type IAllowLetterProps = {
-    letters: IRange<string> | string
-    allowUppercase: boolean
-}
-
-export type AllowNumberProps = {
-    numbers: IRange<number> | number
-}
-
-export type IAllowSpecialCharacterProps = {
-    caracters: string[]
 }
