1b743976efc2e9b8f790dca4753babae963a4feb
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-25 22:37:31 -0300

    :sparkles: create: interface - IFormaterRules

--- src/@types/formater.d.ts
+++ src/@types/formater.d.ts
@@ -1,7 +1,15 @@
 import { IRange } from './range';
+import { apply } from '../verifier/apply';
+export interface IFormaterRules {
+    GROUP?: IGroupProps
+    ALLOW_LETTER?: {letters: IRange<string> | string, allowUppercase: boolean}
+    ALLOW_NUMBER?: {numbers: IRange<number> | number}
+    ALLOW_SPECIAL_CHARACTER?: {caracters: string[]}
+}
 export interface IFormater {
-    GROUP?: (constent: string) => string
+    GROUP?: (content: string, range: IRange<number>) => string
     ALLOW_LETTER?: (letters: IRange<string> | string, allowUppercase: boolean) => string
     ALLOW_NUMBER?: (numbers: IRange<number> | number) => string
     ALLOW_SPECIAL_CHARACTER?: (caracters: string[]) => string
 }
+
